e92b34b998c05c2eef91f73f7d22ffb04bde7a08
Grishma <grishmakachhadiya095@gmail.com>
2023-01-05 16:56:07 +0530

    first commit

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string name= Console.ReadLine();
+Console.WriteLine("This is my name :      {0}" , name);
