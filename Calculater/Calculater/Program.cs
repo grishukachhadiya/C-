815000039a67cb3325addac5d82d6f21a21a05f0
Grishma <grishmakachhadiya095@gmail.com>
2023-01-07 00:00:42 +0530

    second commit

--- Calculater/Calculater/Program.cs
+++ Calculater/Calculater/Program.cs
@@ -1,2 +1,49 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+float num1 = 0; float num2 = 0;
+
+Console.WriteLine("\t\t\t   Calculator");
+Console.WriteLine("\t\t\t -------------------\n");
+
+
+Console.WriteLine("Enter no. 1 : ");
+num1 = float.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter no. 2 : ");
+num2 = float.Parse(Console.ReadLine());
+
+
+Console.WriteLine("\n Chooe Operations");
+Console.WriteLine("1. add");
+Console.WriteLine("2. sub");
+Console.WriteLine("3. mul");
+Console.WriteLine("4. div");
+
+
+switch(Console.ReadLine())
+{
+    case "1":
+        Console.WriteLine(" result : " + (num1 + num2));
+        break;
+
+    case "2":
+        Console.WriteLine(" result : " + (num1 - num2));
+        break;
+
+    case "3":
+        Console.WriteLine(" result : " + (num1 * num2));
+        break;
+
+    case "4":
+        Console.WriteLine(" result : " + (num1 / num2));
+        break;
+    default: Console.WriteLine("This number is not a Correct Number");
+
+        break;
+
+}
+
+
+
+
+
+
